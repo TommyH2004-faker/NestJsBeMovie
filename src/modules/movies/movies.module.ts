288a1d022b8678e4f17252088cc3ac225858b2ae
TommyH <hiept81331@gmail.com>
2025-11-02 15:16:24 +0700

    UPDATE FILE

--- src/modules/movies/movies.module.ts
+++ src/modules/movies/movies.module.ts
@@ -6,7 +6,7 @@ import { Movie } from '../../entity/movie.entity';
 import { Review } from '../../entity/review.entity';
 import { Comment } from '../../entity/comment.entity';
 import { Genre } from '../../entity/genre.entity';
-import { Favorite } from '@src/entity/favorite.entity';
+import { Favorite } from '../../entity/favorite.entity';
 
 @Module({
   imports: [
--- src/modules/movies/movies.service.ts
+++ src/modules/movies/movies.service.ts
@@ -5,7 +5,8 @@ import { Movie } from '../../entity/movie.entity';
 import { Review } from '../../entity/review.entity';
 import { Comment } from '../../entity/comment.entity';
 import { Genre } from '../../entity/genre.entity';
-import { Favorite } from '@src/entity/favorite.entity';
+import { Favorite } from '../../entity/favorite.entity';
+
 @Injectable()
 export class MoviesService {
   constructor(
