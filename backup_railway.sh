5db129121f85072d4e9d3183500eb27ac8506a2e
TommyH <hiept81331@gmail.com>
2025-11-03 10:43:43 +0700

    backup dữ liệu định kì

---
+++ backup_railway.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Thư mục lưu backup
+mkdir -p ./backups
+BACKUP_FILE="./backups/railway_backup_$(date +%Y-%m-%d_%H-%M-%S).sql.gz"
+
+# Sử dụng biến môi trường thay vì viết cứng
+mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USER -p$DB_PASSWORD $DB_NAME --single-transaction --routines --triggers | gzip > $BACKUP_FILE
+
+echo "Backup thành công: $BACKUP_FILE"
